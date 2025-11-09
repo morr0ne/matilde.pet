5a43f0e270667a93cca91a16dda964e60e1fbe6b
Matilde Morrone <contact@morrone.dev>
2025-11-09 15:46:44 +0100

    Fresh start :3

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,8 @@
-import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import icon from 'astro-icon';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://morrone.dev',
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: 'load',
-  },
-  integrations: [icon(), sitemap()],
-  markdown: {
-    shikiConfig: {
-      theme: 'light-plus',
-    },
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	site: 'https://matilde.pet',
+	integrations: [sitemap()],
 });
--- src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = 'Matilde Morrone';
-export const SITE_DESCRIPTION = "Mati's personal website";
